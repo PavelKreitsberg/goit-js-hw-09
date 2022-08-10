d6f0bea01bed6c87e162c720b82e95490c899604
Pavel Kreitsberg <pavel.kreitsberg@gmail.com>
2022-08-10 11:12:31 +0300

    Solve Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
 
+let isActive = false;
+
+let timerId = null;
+
+const getRandomHexColor = () => `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const startChanging = (event) => {
+    if (isActive) {
+        return;
+    }
+
+    isActive = true;
+
+    refs.body.style = `background-color: ${getRandomHexColor()}`
+
+    timerId = setInterval(() => { refs.body.style = `background-color: ${getRandomHexColor()}` }, 1000)
+}
+
+const stopChanging = (event) => {
+    isActive = false;
+    clearInterval(timerId);
+}
+
+
+refs.startBtn.addEventListener('click', startChanging);
+
+refs.stopBtn.addEventListener('click', stopChanging)
